16b67d20caec891deb2471e07d7d8afbd01577e6
elon1te9 <elon1te9@gmail.com>
2026-04-13 08:33:16 +0300

    Initial commit

---
+++ RustyMangoAPI/Controllers/GameController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RustyMangoAPI.Interfaces;
+using RustyMangoAPI.Requests;
+
+namespace RustyMangoAPI.Controllers
+{
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet("getProgress")]
+        public async Task<IActionResult> GetProgress(int userId)
+        {
+            return await _gameService.GetProgress(userId);
+        }
+
+        [HttpPost("completeLevel")]
+        public async Task<IActionResult> CompleteLevel(CompleteLevelRequest request)
+        {
+            return await _gameService.CompleteLevel(request);
+        }
+
+        [HttpGet("getLeaderboard")]
+        public async Task<IActionResult> GetLeaderboard()
+        {
+            return await _gameService.GetLeaderboard();
+        }
+    }
+}
+
---
+++ RustyMangoAPI/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RustyMangoAPI.Interfaces;
+using RustyMangoAPI.Requests;
+
+namespace RustyMangoAPI.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("registrationUser")]
+        public async Task<IActionResult> Registration([FromBody] RegisterRequest request)
+        {
+            return await _userService.Register(request);
+        }
+
+        [HttpPost]
+        [Route("authUser")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            return await _userService.Login(request);
+        }
+
+        [HttpGet("getUser")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            return await _userService.GetUser(id);
+        }
+
+    }
+}
